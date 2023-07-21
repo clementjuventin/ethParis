7f307e31edbd0175d1960c6abca8a36158fbf652
Coltcivers <girardhilal@gmail.com>
2023-07-21 23:36:50 +0200

    edit

--- indexer/main.go
+++ indexer/main.go
@@ -15,18 +15,40 @@ import (
 
 func checkSupportInterface(client *w3.Client, address common.Address, interfaceId string) bool {
 
-	funcSupportsInterface := w3.MustNewFunc("supportsInterface(bytes4 interfaceId)", "bool")
+	funcSupportsInterface := w3.MustNewFunc("supportsInterface(bytes4)", "bool")
 
 	var resp bool
 
+	value := [4]byte{}
+	copy(value[:], []byte(interfaceId))
+
 	err := client.Call(
-		eth.CallFunc(funcSupportsInterface, address, w3.B(interfaceId)).Returns(&resp),
+		//eth.CallFunc(funcSupportsInterface, address, w3.B(interfaceId)).Returns(&resp),
+		eth.CallFunc(funcSupportsInterface, address, value).Returns(&resp),
 	)
 
 	if err != nil {
-		log.Fatalln("err w3 :", err)
+		log.Println("err w3 :", err)
+		return false
 	}
 	return resp
+}
+
+func checkOwner(client *w3.Client, address common.Address) common.Address {
+
+	funcSupportsInterface := w3.MustNewFunc("owner()", "address")
+	var owner common.Address
+
+	err := client.Call(
+		//eth.CallFunc(funcSupportsInterface, address, w3.B(interfaceId)).Returns(&resp),
+		eth.CallFunc(funcSupportsInterface, address).Returns(&owner),
+	)
+
+	if err != nil {
+		log.Println("err w3 :", err)
+		return common.Address{}
+	}
+	return owner
 }
 
 func main() {
@@ -41,6 +63,12 @@ func main() {
 	client2 := w3.MustDial(endpoint)
 
 	log.Println("passed both conn")
+
+	resp := checkSupportInterface(client2, common.HexToAddress("0xa02573c4ad15c16b48f10842aac9c9ea405b65a3"), "0x80ac58cd")
+	log.Println("implements interface :", resp)
+
+	owner := checkOwner(client2, common.HexToAddress("0xa02573c4ad15c16b48f10842aac9c9ea405b65a3"))
+	log.Println("owner :", owner)
 
 	/*
 		newHeads := make(chan *types.Header)
@@ -88,8 +116,6 @@ func main() {
 		if err != nil {
 			log.Fatalln(err)
 		}
-		log.Println("////////")
-		log.Println("Transactions for block :")
 		for pos, tx := range block.Transactions() {
 			if tx.To() == nil {
 				log.Println("tx pos / hash :", pos, "|", tx.Hash())
