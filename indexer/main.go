36b5221e3e792aa40a420cc16eed60985bd46943
Coltcivers <girardhilal@gmail.com>
2023-07-22 14:13:03 +0200

    edit

--- indexer/main.go
+++ indexer/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"log"
 	"math/big"
+	"sync"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -12,123 +14,155 @@ import (
 	"github.com/metachris/eth-go-bindings/erc165"
 )
 
+/*
 var EVT_TRANSFER = crypto.Keccak256Hash([]byte("Transfer(address,address,uint256)"))
 var EVT_MINT = crypto.Keccak256Hash([]byte("Mint(address,uint256)"))
 var EVT_BURN = crypto.Keccak256Hash([]byte("Burn(address,uint256)"))
 
-var EVENTS_SIG = []common.Hash{
-	EVT_TRANSFER,
-	EVT_MINT,
-	EVT_BURN,
-}
-var NIL_ADDR = common.HexToAddress("0x0")
+	var EVENTS_SIG = []common.Hash{
+		EVT_TRANSFER,
+		EVT_MINT,
+		EVT_BURN,
+	}
 
-func weiToEther(wei *big.Int) *big.Float {
-	ether := new(big.Float)
-	ether.SetString(wei.String())
-	ether = ether.Quo(ether, big.NewFloat(1000000000000000000))
-	return ether
+	func weiToEther(wei *big.Int) *big.Float {
+		ether := new(big.Float)
+		ether.SetString(wei.String())
+		ether = ether.Quo(ether, big.NewFloat(1000000000000000000))
+		return ether
+	}
+
+	func eventChecker(tx *types.Transaction, client *ethclient.Client) (common.Address, error) {
+		// Get tx receipt
+		receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
+		if err != nil {
+			return common.Address{}, err
+		}
+
+		for _, vLog := range receipt.Logs {
+			for _, topic := range vLog.Topics {
+
+				if topic == EVT_TRANSFER {
+					log.Println("Event found ! -> Transfer")
+					if vLog.Topics[1].Hex()[26:] == common.HexToAddress("0x0").Hex()[2:] {
+						log.Println("Mint @@@@@@@@@@")
+						log.Println("Token id :", vLog.Topics[3].Big().Uint64())
+						// Wei to ether
+						txValue := weiToEther(tx.Value())
+
+						log.Println("mint price :", txValue, "ETH")
+						log.Println("Tx :", tx.Hash().Hex())
+					}
+				}
+				if topic == EVT_MINT {
+					log.Println("Event found ! -> Mint")
+					log.Println("Tx :", tx.Hash().Hex())
+				}
+				if topic == EVT_BURN {
+					log.Println("Event found ! -> Burn")
+				}
+			}
+		}
+		return common.Address{}, nil
+	}
+*/
+type Data struct {
+	Collections      []common.Address
+	CollectionsMutex *sync.Mutex
 }
 
 func detectERC721Deployment(tx *types.Transaction, client *ethclient.Client) (common.Address, error) {
 	signer := types.LatestSignerForChainID(tx.ChainId())
 	sender, err := signer.Sender(tx)
 	if err != nil {
-		return NIL_ADDR, err
+		return common.Address{}, err
 	}
 	contractAddress := crypto.CreateAddress(sender, tx.Nonce())
 
 	contract, err := erc165.NewErc165(contractAddress, client)
 	if err != nil {
-		return NIL_ADDR, err
+		return common.Address{}, err
 	}
 	isERC721, err := contract.SupportsInterface(nil, [4]byte{0x80, 0xac, 0x58, 0xcd})
 	if err == nil && isERC721 {
 		log.Println("ERC721 contract found !", contractAddress.Hex())
 		return contractAddress, nil
 	}
-	return NIL_ADDR, err
+	return common.Address{}, err
 }
 
-func eventChecker(tx *types.Transaction, client *ethclient.Client) (common.Address, error) {
-	// Get tx receipt
-	receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
-	if err != nil {
-		return NIL_ADDR, err
-	}
-
-	for _, vLog := range receipt.Logs {
-		for _, topic := range vLog.Topics {
-
-			if topic == EVT_TRANSFER {
-				log.Println("Event found ! -> Transfer")
-				if vLog.Topics[1].Hex()[26:] == common.HexToAddress("0x0").Hex()[2:] {
-					log.Println("Mint @@@@@@@@@@")
-					log.Println("Token id :", vLog.Topics[3].Big().Uint64())
-					// Wei to ether
-					txValue := weiToEther(tx.Value())
-
-					log.Println("mint price :", txValue, "ETH")
-					log.Println("Tx :", tx.Hash().Hex())
-				}
-			}
-			if topic == EVT_MINT {
-				log.Println("Event found ! -> Mint")
-				log.Println("Tx :", tx.Hash().Hex())
-			}
-			if topic == EVT_BURN {
-				log.Println("Event found ! -> Burn")
-			}
-		}
-	}
-	return NIL_ADDR, nil
-}
-
-func blockAnalizer(block *types.Block, client *ethclient.Client) {
-	transactions := block.Transactions()
-	for _, tx := range transactions {
+func blockAnalizer(block *types.Block, client *ethclient.Client, data *Data) {
+	for _, tx := range block.Transactions() {
 		// if it's a deployment transaction, the to field will be nil
 		if tx.To() == nil {
-			_, err := detectERC721Deployment(tx, client)
+			addr, err := detectERC721Deployment(tx, client)
 			if err != nil {
-				log.Fatalln(err)
+				continue
+			}
+			if addr != (common.Address{}) {
+				data.CollectionsMutex.Lock()
+				data.Collections = append(data.Collections, addr)
+				data.CollectionsMutex.Unlock()
 			}
 		}
-
-		_, err := eventChecker(tx, client)
-		if err != nil {
-			log.Fatalln(err)
-		}
 	}
+}
+
+func query(client *ethclient.Client, blockNb int, data *Data) {
+	block, err := client.BlockByNumber(context.Background(), big.NewInt(int64(blockNb)))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("got block :", block.Number())
+	blockAnalizer(block, client, data)
+}
+
+func manager(client *ethclient.Client, blockHeight int) *Data {
+
+	data := new(Data)
+	data.CollectionsMutex = new(sync.Mutex)
+
+	for i := 0; i < blockHeight; i++ {
+		if (i+1)%75== 0 {
+			time.Sleep(time.Second * 1)
+			log.Println("batch completed at :", i)
+		}
+		go query(client, i, data)
+	}
+
+	return data
 }
 
 func main() {
-	endpoint := "wss://linea-mainnet.infura.io/ws/v3/11135f07f5c84261a8887926742776c6"
+	endpoint := "wss://linea-mainnet.infura.io/ws/v3/531ec7a2e96642ceac54c241c236c580"
 	client, err := ethclient.Dial(endpoint)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	// w3client := w3.MustDial(endpoint)
+	contract, err := erc165.NewErc165(common.HexToAddress("0xb62c414abf83c0107db84f8de1c88631c05a8d7b"), client)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	isERC721, err := contract.SupportsInterface(nil, [4]byte{0x80, 0xac, 0x58, 0xcd})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("test punks :", isERC721)
 
-	blockSync := big.NewInt(4541)
-	// Syncing
-	for {
-		block, err := client.BlockByNumber(context.Background(), blockSync)
+	/*
+		height, err := client.BlockNumber(context.Background())
 		if err != nil {
 			log.Fatalln(err)
 		}
-		log.Println("block number :", block.Number())
-		blockAnalizer(block, client)
+		log.Println("chain height :", height)
 
-		blockNumber, err := client.BlockNumber(context.Background())
-		if err != nil {
-			log.Fatalln(err)
-		}
-		log.Println("block number :", block.Number())
-
-		if blockNumber == block.Number().Uint64() {
-			return
-		}
-		blockSync.Add(blockSync, big.NewInt(1))
+		data := manager(client, int(height))
+	*/
+	height := 2000
+	data := manager(client, height)
+	log.Println("data up to ", height, "collections amount found :", len(data.Collections))
+	log.Println("displaying collecitons addresses :")
+	for _, collec := range data.Collections {
+		log.Println("|", collec)
 	}
 }
