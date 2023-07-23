3cd73fe24f12f1da42c2c130ad95b4e61c830366
Clément Juventin <clementjuventin@gmail.com>
2023-07-23 02:38:00 +0200

    api

--- api/api.go
+++ api/api.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/gin-gonic/gin"
@@ -44,9 +45,14 @@ type ERC721Struct struct {
 func main() {
 	router := gin.Default()
 	router.POST("/nft/history/:collection/:tokenId", getNftHistory)
-	router.POST("/nft/:addr", getAddressNfts)
+	router.POST("/nft/:collection/:tokenId", getNftData)
 
-	router.POST("/userTx/:addr", getUserTransactions)
+	router.POST("/collection/:addr", getCollectionNfts)
+	router.POST("/collection/history/:addr", getCollectionHistory)
+	router.POST("/collection/stats/:addr", getCollectionStats)
+
+	router.POST("/address/history/:addr", getAddressHistory)
+	router.POST("/address/:addr", getAddressNfts)
 
 	router.Run("localhost:8080")
 }
@@ -58,7 +64,6 @@ func getDbInstance() (database *sql.DB, e error) {
 	}
 	return db, nil
 }
-
 func getNftHistory(c *gin.Context) {
 	collection := c.Param("collection")
 	tokenId := c.Param("tokenId")
@@ -76,7 +81,7 @@ func getNftHistory(c *gin.Context) {
 	collection = strings.ToLower(collection)
 	tokenId = strings.ToLower(tokenId)
 
-	rows, err := db.Query("SELECT * FROM ERC721Tx WHERE collection = $1 AND token_id = $2 ORDER BY id DESC LIMIT 100 OFFSET $3", collection, tokenId, offset)
+	rows, err := db.Query("SELECT * FROM ERC721Tx WHERE collection = $1 AND token_id = $2 ORDER BY timestamp DESC LIMIT 100 OFFSET $3", collection, tokenId, offset)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -116,7 +121,7 @@ func getAddressNfts(c *gin.Context) {
 		return
 	}
 
-	rows, err := db.Query("SELECT * FROM ERC721 WHERE owner = $1 ORDER BY id DESC LIMIT 100 OFFSET $2", address, offset)
+	rows, err := db.Query("SELECT * FROM ERC721 WHERE owner = $1 ORDER BY mint_timestamp DESC LIMIT 100 OFFSET $2", address, offset)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -125,7 +130,6 @@ func getAddressNfts(c *gin.Context) {
 	var nfts []ERC721Struct
 	for rows.Next() {
 		var nft ERC721Struct
-		// The first parameter is the id, we don't need it
 		owner := ""
 		collection := ""
 		err = rows.Scan(&nft.MintTimestamp, &nft.MintBlockNumber, &nft.MintTxHash, &nft.URI, &nft.TokenId, &collection, &owner)
@@ -140,8 +144,42 @@ func getAddressNfts(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"data": nfts})
 }
+func getCollectionNfts(c *gin.Context) {
+	address := c.Param("addr")
+	offset := c.Param("offset")
+	if offset == "" {
+		offset = "0"
+	}
 
-func getUserTransactions(c *gin.Context) {
+	db, err := getDbInstance()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	rows, err := db.Query("SELECT * FROM ERC721 WHERE collection = $1 ORDER BY mint_timestamp DESC LIMIT 100 OFFSET $2", address, offset)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	var nfts []ERC721Struct
+	for rows.Next() {
+		var nft ERC721Struct
+		owner := ""
+		collection := ""
+		err = rows.Scan(&nft.MintTimestamp, &nft.MintBlockNumber, &nft.MintTxHash, &nft.URI, &nft.TokenId, &collection, &owner)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		nft.Collection = common.HexToAddress(collection)
+		nft.Owner = common.HexToAddress(owner)
+		nfts = append(nfts, nft)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": nfts})
+}
+func getAddressHistory(c *gin.Context) {
 	address := c.Param("addr")
 	offset := c.Param("offset")
 	if offset == "" {
@@ -155,7 +193,7 @@ func getUserTransactions(c *gin.Context) {
 		return
 	}
 
-	rows, err := db.Query("SELECT * FROM ERC721Tx WHERE from_addr = $1 OR to_addr = $1 ORDER BY id DESC LIMIT 100 OFFSET $2", address, offset)
+	rows, err := db.Query("SELECT * FROM ERC721Tx WHERE from_addr = $1 OR to_addr = $1 ORDER BY timestamp DESC LIMIT 100 OFFSET $2", address, offset)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -165,7 +203,6 @@ func getUserTransactions(c *gin.Context) {
 	for rows.Next() {
 		var tx ERC721TxStruct
 		id := 0
-		// The first parameter is the id, we don't need it
 		fromAddress := ""
 		toAddress := ""
 		collection := ""
@@ -181,4 +218,147 @@ func getUserTransactions(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"data": txs})
+}
+func getCollectionHistory(c *gin.Context) {
+	address := c.Param("addr")
+	offset := c.Param("offset")
+	if offset == "" {
+		offset = "0"
+	}
+
+	db, err := getDbInstance()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	rows, err := db.Query("SELECT * FROM ERC721Tx WHERE collection = $1 ORDER BY timestamp DESC LIMIT 100 OFFSET $2", address, offset)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var txs []ERC721TxStruct
+	for rows.Next() {
+		var tx ERC721TxStruct
+		id := 0
+		fromAddress := ""
+		toAddress := ""
+		collection := ""
+		err = rows.Scan(&id, &tx.Timestamp, &tx.BlockNumber, &tx.TxHash, &tx.Tag, &fromAddress, &toAddress, &tx.Value, &tx.TokenId, &collection)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		tx.FromAddr = common.HexToAddress(fromAddress)
+		tx.ToAddr = common.HexToAddress(toAddress)
+		tx.Collection = common.HexToAddress(collection)
+		txs = append(txs, tx)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": txs})
+}
+func getNftData(c *gin.Context) {
+	collection := c.Param("collection")
+	tokenId := c.Param("tokenId")
+
+	db, err := getDbInstance()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	collection = strings.ToLower(collection)
+	tokenId = strings.ToLower(tokenId)
+
+	rows, err := db.Query("SELECT * FROM ERC721 WHERE collection = $1 AND token_id = $2", collection, tokenId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var nft ERC721Struct
+	for rows.Next() {
+		owner := ""
+		collection := ""
+		err = rows.Scan(&nft.MintTimestamp, &nft.MintBlockNumber, &nft.MintTxHash, &nft.URI, &nft.TokenId, &collection, &owner)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		nft.Collection = common.HexToAddress(collection)
+		nft.Owner = common.HexToAddress(owner)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": nft})
+}
+func getCollectionStats(c *gin.Context) {
+	address := c.Param("addr")
+	since := c.Param("since")
+	if since == "" {
+		// Date.now - 24h
+		since = strings.Split(time.Now().Add(-24*time.Hour).String(), " ")[0]
+	}
+	db, err := getDbInstance()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	const ownerCountQuery = `SELECT COUNT(DISTINCT owner) FROM ERC721 WHERE collection = $1`
+	const txCountSinceQuery = `SELECT COUNT(DISTINCT HASH) FROM ERC721Tx WHERE collection = $1 AND timestamp > $2`
+	// const volumeSinceQuery = `SELECT SUM(value)::numeric FROM ERC721Tx WHERE collection = $1 AND timestamp > $2`
+
+	oCount := 0
+	tCount := 0
+	// vCount := 0
+
+	rows, err := db.Query(ownerCountQuery, address)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&oCount)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
+	rows, err = db.Query(txCountSinceQuery, address, since)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&tCount)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
+	// rows, err = db.Query(volumeSinceQuery, address, since)
+	// if err != nil {
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	// 	return
+	// }
+	// defer rows.Close()
+
+	// for rows.Next() {
+	// 	err = rows.Scan(&vCount)
+	// 	if err != nil {
+	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	// 		return
+	// 	}
+	// }
+	c.JSON(http.StatusOK, gin.H{"data": gin.H{"ownerCount": oCount, "txCount": tCount, "volume": 0}})
 }
